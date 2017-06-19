17acfd2858f654c5fb7c8fae3994b7c6f6c6245a
BG <bg4444@list.ru>
2017-06-19 07:39:56 +0300

    add usage

--- xml_inserter.h
+++ xml_inserter.h
@@ -10,7 +10,13 @@ class InvalidInsertMode
 };
 
 void appendTextChild(QDomDocument& doc, QDomElement& parent,const QString& name,const QString& value);
+
 QDomElement locate(QDomDocument& doc, QDomElement node, const QString& path, const bool alwaysAdd);
+
+//Insert attribure or value into DOM by path. 
+//'a' insert("a/foo/bar","smth") for attribute (<foo bar="smth"/>)
+//'v' insert("v/foo","smth") for attribute (<foo>smth</foo>)
+
 void insert(QDomDocument& doc, QDomElement node,const QString& path, const QString& value);
 
 #endif // XML_INSERTER_H

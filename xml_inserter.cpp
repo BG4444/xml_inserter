9f970bfe3c0575c78f6bea0cd528ea51261b8a98
BG <bg4444@list.ru>
2017-07-09 23:29:47 +0300

    add funcs

--- xml_inserter.cpp
+++ xml_inserter.cpp
@@ -1,4 +1,6 @@
 #include "xml_inserter.h"
+#include <QTextCodec>
+#include <SerException.h>
 
 void appendTextChild(QDomDocument& doc, QDomElement& parent,const QString& name,const QString& value)
 {
@@ -94,3 +96,108 @@ void insert(QDomDocument& doc, QDomElement node,const QString& path, const QStri
     }
 }
 
+QDomElement findNode(const QString& tag, const QString& path, const int posStart, const QDomNodeList& children, const QTextCodec *codecConsole)
+{
+    QString nodesList;
+    int i=children.size();
+    while(--i>=0)
+    {
+        const auto& cur=children.at(i);
+        nodesList += QString("[%1] '%2' %3\n").arg(i).arg(cur.nodeName()).arg(cur.isElement());
+        if(cur.nodeName()==tag && cur.isElement())
+        {
+            break;
+        }
+    }
+
+    if(i==-1)
+    {
+        const auto msgText=codecConsole->fromUnicode(QString("\nXML Path not found!\nPath: %1\ntag %2\nat pos %3\nExisting nodes is\n%4").arg(path).arg(tag).arg(posStart).arg(nodesList));
+
+        throw SerException(__FILE__,__LINE__,msgText.data());
+    }
+    else
+    {
+        return children.at(i).toElement();
+    }
+
+}
+
+QString readNode(QDomElement node, const QString &path, const QTextCodec *codecConsole)
+{
+    const QChar delim('/');
+
+    int posDelim=path.indexOf(delim,0);
+
+    if(posDelim!=1)
+    {
+        throw SerException(__FILE__,__LINE__);
+    }
+
+    const char mode = path[0].toLatin1();
+
+    while(true)
+    {
+        const int posStart=posDelim+1;
+
+        posDelim=path.indexOf(delim,posStart);
+
+        const int tagEnd=(posDelim == -1) ? path.length() : posDelim;
+
+        const int tagLength=tagEnd - posStart;
+
+        const QString tag=QStringRef(&path,posStart, tagLength).toString();
+
+        if(posDelim==-1)
+        {
+            switch(mode)
+            {
+              case 'a':
+              {
+                if(node.attributes().contains(tag))
+                {
+                    return node.attribute(tag);
+                }
+                else
+                {
+                    const auto msgText=codecConsole->fromUnicode(QString("\nXML Attribute not found!\nPath: %1\ntag %2\nat pos %3\n").arg(path).arg(tag).arg(posStart));
+                    throw SerException(__FILE__,__LINE__,msgText.data());
+                }
+              }
+              case 'v':
+              {
+                   return findNode(tag, path,posStart, node.childNodes(),codecConsole).text();
+              }
+              default:
+              {
+                   throw SerException(__FILE__,__LINE__);
+              }
+            }
+        }
+        node = findNode(tag, path,posStart, node.childNodes(),codecConsole);
+    }
+}
+
+QDomElement harvestElement(QDomElement node, const QString &path, const QTextCodec *codecConsole)
+{
+    const QChar delim('/');
+
+    int posDelim=-1;
+
+    do
+    {
+        const int posStart=posDelim+1;
+
+        posDelim=path.indexOf(delim,posStart);
+
+        const int tagEnd=(posDelim == -1) ? path.length() : posDelim;
+
+        const int tagLength=tagEnd - posStart;
+
+        const QString tag=QStringRef(&path,posStart, tagLength).toString();
+
+        node = findNode(tag, path,posStart, node.childNodes(),codecConsole);
+    }
+    while(posDelim!=-1);
+    return node;
+}
--- xml_inserter.h
+++ xml_inserter.h
@@ -19,4 +19,7 @@ QDomElement locate(QDomDocument& doc, QDomElement node, const QString& path, con
 
 void insert(QDomDocument& doc, QDomElement node,const QString& path, const QString& value);
 
+QString readNode(QDomElement node, const QString &path, const QTextCodec *codecConsole);
+QDomElement harvestElement(QDomElement node, const QString &path, const QTextCodec *codecConsole);
+
 #endif // XML_INSERTER_H
